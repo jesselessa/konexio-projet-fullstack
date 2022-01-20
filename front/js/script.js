587ddb162ba7f86881db82682f963ee461844ab0
jess <jessica.elessa@gmail.com>
2022-01-20 17:38:05 +0100

    fix some of bugs with the spinner

--- front/js/script.js
+++ front/js/script.js
@@ -2,31 +2,9 @@
 
 // On va écrire notre code sous forme de fonction call-back de façon à s'assurer qu'il s'exécute une fois que nos scripts JS seront chargés
 
-
-// // Function getAllCountries
-
-//     function getAllCountries() {
-//         $("#btnShowData").click(function () {            
-//             $.ajax({
-//                 url: `https://restcountries.com/v3.1/all`,success: function (data) {                             
-//                 let countries = data.map((country)=> {                    
-//                     return `<li><h3>${country.name.common}</h3></li>
-//                                 <ul class="infos">
-//                                     <li>Capitale : ${country.capital}</li>
-//                                     <li>Continent : ${country.continents}</li>                            
-//                                     <li>Langue : </li>                       
-//                                     <li>Monnaie : </li>                       
-//                                 </ul>`
-//                 }); 
-//                 // Puces sous-liste
-//                 $("#countriesList").css("list-style-type","none").html(countries.join(""));    
-//                 },
-//             });
-//         });
-//     }
-
 // getAllCountries();
 
+// });
 
 $().ready(() => {
     // Fonction pour afficher le spinner
@@ -36,24 +14,24 @@ $().ready(() => {
 </div>`)
    }
     
-    // Fonction pour cacher le spinner
-    function hideSpinner() {
-        $(".spinner-border").remove()
-    }
+// Fonction pour cacher le spinner
+function hideSpinner() {
+    $(".spinner-border").remove()
+}
     // Fonction pour réinitialiser la page
     function reset() {
         $("li").remove()
         showSpinner()
         
-        // $.ajax({
-        //     url: "https://restcountries.com/v3.1/all",
-        //     success: function(countries) {
-        //         countries.forEach(country => {
-        //             $("#countriesList").append(
-        //                 `<li class="list-group-item"><p>Name : ${country.name.common}<br />Capital : ${country.capital}
-        //              <br />Continent : ${country.region}</p></li>`
-        //             )
-        //         });
+        $.ajax({
+            url: "https://restcountries.com/v3.1/all",
+            success: function(countries) {
+                countries.forEach(country => {
+                    $("#countriesList").append(
+                        `<li class="list-group-item"><p>Name : ${country.name.common}<br />Capital : ${country.capital}
+                     <br />Continent : ${country.region}</p></li>`
+                    )
+                });
             }
         })
 
@@ -68,14 +46,11 @@ $().ready(() => {
                                                 <li>Langue : </li>                       
                                                 <li>Monnaie : </li>                       
                                             </ul>`
-                            })
-                            // Puces sous-liste
-                            $("#countriesList").html(countries.join(""))
-                            .then(() => {
+                            }).then(() => {
             hideSpinner();
-    //     })}
+        })}
 
-    // })};
+    })};
 
     // Appel de la fonction d'ouverture de la page à chaque affichage
     reset()
@@ -102,38 +77,24 @@ $().ready(() => {
         }
 
         $.ajax({
-            // Importation de l'API REST Countries
-
+            // Importation de l'API rescountries
             url: url + $("#countryForm").val(),
-            // success: function(countries) {
-            //     countries.forEach(country => {
+            success: function(countries) {
+                countries.forEach(country => {
                     
-            //         $("#countriesList").append(
-            //             `<li class="list-group-item"><p>Name : ${country.name.common}<br />Capital : ${country.capital} <br />Continent : ${country.region}</p></li>`
-            //         )
-
-            success: function (data){                             
-                let countries = data.map((country)=> {                    
-                    return `<li><h3>${country.name.common}</h3></li>
-                                            <ul class="infos">
-                                                <li>Capitale : ${country.capital}</li>
-                                                <li>Continent : ${country.continents}</li>                         
-                                               
-                                            </ul>`
-                            })
-                        }
-                        
-    //             });
-    //         },
-    //     }
-    // })
-    .then(() => {
+                    $("#countriesList").append(
+                        `<li class="list-group-item"><p>Name : ${country.name.common}<br />Capital : ${country.capital} <br />Continent : ${country.region}</p></li>`
+                    )
+                    console.log(country.name.common)
+                });
+            },
+        }).then(() => {
             hideSpinner()
         })
-    });
+    };
 
 // Appel de la fonction getAllCountries à chaque "clic" sur le bouton ShowData 
     $("#btnShowData").click(() => {
         getAllCountries();
-    })}
-})}})
+    })
+});
